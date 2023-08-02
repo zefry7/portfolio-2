cf74ce0c47cd9326ccf2c8a048dbc63e3ba43124
nikita petrov <petrov.nikita702@mail.ru>
2023-08-02 11:41:49 +0300

    completed project

--- script/script.js
+++ script/script.js
@@ -12,4 +12,28 @@ window.addEventListener("resize", () => {
         document.body.classList.remove("block-lock");
         menuBurger.classList.remove("menu-burger_active");
     }
+});
+
+const menuLinks = document.querySelectorAll(".header-content__link[data-goto]");
+menuLinks.forEach(link => {
+    link.addEventListener("click", function(e) {
+        const el = e.target;
+        headerContentMenu.classList.remove("header-content__menu_visibility");
+        document.body.classList.remove("block-lock");
+        menuBurger.classList.remove("menu-burger_active");
+        
+        if(el.dataset.goto != "title-content") {
+            const gotoBlock = document.getElementsByClassName(el.dataset.goto)[0];
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.scrollY - 30;
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            })
+        } else {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            })
+        }
+    })
 })
